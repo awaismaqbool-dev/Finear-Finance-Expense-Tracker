48c18d57a4d4f864c4af1269d10ce1643531d18f
Awais Maqbool <awi.ah142@gmail.com>
2026-02-13 16:06:07 +0500

    fix: case sensitive import paths again

--- Frontend/Finear/src/pages/Home/HowItWorks.jsx
+++ Frontend/Finear/src/pages/Home/HowItWorks.jsx
@@ -1,6 +1,6 @@
 import React from "react";
 import Content from "../../components/common/Content";
-import Steps from "./components/steps";
+import Steps from "./components/Steps";
 
 function HowItWorks() {
   return (
